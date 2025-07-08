d9bbcbb1333f18e50415c56a7a28b9ab09937358
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-07-08 20:05:14 +0530

    some changes done

--- src/app.js
+++ src/app.js
@@ -4,12 +4,7 @@ import coockieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 //Common middlewares
 app.use(express.json({ limit: "16kb" }));
