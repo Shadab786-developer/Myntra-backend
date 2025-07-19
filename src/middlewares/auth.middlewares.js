d792eb9541d18dd59060aff18baba7c2bda1d190
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-07-19 07:07:52 +0530

    Middleware are updated

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Registration } from "../models/register.models.js";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   //storing the token which is comming into request of header
@@ -24,6 +25,48 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
     console.log("Decoded token:", decodedToken);
     //assigned the token to user variable
     const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    console.log("User found:", user);
+
+    // Verify the user
+    if (!user) {
+      throw new ApiError(401, "Unauthorized access, user not found");
+    }
+
+    req.user = user; // Attach user to request object
+
+    next(); // Proceed to the next middleware or route handler
+  } catch (error) {
+    console.log("JWT verification failed:", error);
+    throw new ApiError(
+      401,
+      error?.message || "Unauthorized access, invalid token"
+    );
+  }
+});
+export const verifyRegisterJWT = asyncHandler(async (req, _, next) => {
+  //storing the token which is comming into request of header
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+
+  console.log("Token received:", token);
+  //verify wheather the token was come or not
+  if (!token) {
+    throw new ApiError(401, "Unauthorized access, token is missing");
+  }
+
+  console.log("JWT_SECRET:", process.env.ACCESS_TOKEN_SECRET);
+  console.log("Cookies:", req.cookies);
+  console.log("Authorization header:", req.header("Authorization"));
+  try {
+    //Decoded the token to verify by jwt
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    console.log("Decoded token:", decodedToken);
+    //assigned the token to user variable
+    const user = await Registration.findById(decodedToken?._id).select(
       "-password -refreshToken"
     );
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,7 +9,10 @@ import {
   registerLogout,
   verifyRegisterEmail,
 } from "../controllers/login.controller.js";
-import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import {
+  verifyJWT,
+  verifyRegisterJWT,
+} from "../middlewares/auth.middlewares.js";
 const router = Router();
 
 router.route("/singin").post(singIn);
@@ -17,8 +20,8 @@ router.route("/login").post(verifyJWT, userLogin);
 router.route("/logout").post(verifyJWT, userLogout);
 router.route("/verify-email").post(verifyEmail);
 router.route("/register").post(register);
-router.route("/registerLogin").post(verifyJWT, registerLogin);
-router.route("/registerLogout").post(verifyJWT, registerLogout);
+router.route("/registerLogin").post(verifyRegisterJWT, registerLogin);
+router.route("/registerLogout").post(verifyRegisterJWT, registerLogout);
 router.route("/verifyRegisterEmail").post(verifyRegisterEmail);
 
 export default router;
