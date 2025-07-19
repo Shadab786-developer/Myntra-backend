ed8321b2b7b9e3410f16df1698dcfd2fe72d2d59
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-07-19 08:41:18 +0530

    Middleware are updated

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -63,6 +63,32 @@ const singIn = asyncHandler(async (req, res) => {
   }
 
   try {
+    // Generate a random 6-digit verification code
+    const verificationCode = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+    // Send verification email
+    await sendEmail(
+      email,
+      "Verify your email",
+      `<div style="max-width:400px; margin:0 auto;padding:28px 20px; background:#fff; border-radius:8px; box-shadow:0 2px 8px rgba(0,0,0,0.08); font-family:'Segoe UI',Arial,sans-serif;">
+  <div style="text-align:center;">
+    <img src="https://content.jdmagicbox.com/v2/comp/delhi/p1/011pxx11.xx11.180419233547.s2p1/catalogue/myntra-warehouse-mohammadpur-delhi-warehouses-services-1o8sinohci-250.jpg;" alt="Myntra" style="height:300px; width:300px; margin-bottom:8px;">
+    <h2 style="color:#ff3f6c; margin-bottom:8px;">Verify Your Email</h2>
+    <p style="color:#333; font-size:16px; margin-bottom:24px;">
+      Thank you for signing up with Myntra!<br>
+      Please use the code below to verify your email address.
+    </p>
+    <div style="display:inline-block; background:#fff; color:#ff3f6c; font-size:28px; letter-spacing:6px; border:1px solid #ff3f6c; padding:12px 32px; border-radius:6px; font-weight:bold; margin-bottom:24px;">
+      ${verificationCode}
+    </div>
+    <p style="color:#555; font-size:14px; margin-top:24px;">
+      Didn’t request this? Please ignore this email.<br>
+      <b style="color:blue;">Need help?</b> Contact <a href="mailto:support@myntra.com" style="color:#ff3f6c; text-decoration:none;">support@myntra.com</a>
+    </p>
+  </div>
+</div>`
+    );
     //Create new user
 
     const user = await User.create({
@@ -132,34 +158,6 @@ const userLogin = asyncHandler(async (req, res) => {
   if (!isPasswordValid) {
     throw new ApiError(401, "Invalid password or credentials");
   }
-
-  // Generate a random 6-digit verification code
-  const verificationCode = Math.floor(
-    100000 + Math.random() * 900000
-  ).toString();
-  // Send verification email
-  await sendEmail(
-    email,
-    "Verify your email",
-    `<div style="max-width:400px; margin:0 auto;padding:28px 20px; background:#fff; border-radius:8px; box-shadow:0 2px 8px rgba(0,0,0,0.08); font-family:'Segoe UI',Arial,sans-serif;">
-  <div style="text-align:center;">
-    <img src="https://content.jdmagicbox.com/v2/comp/delhi/p1/011pxx11.xx11.180419233547.s2p1/catalogue/myntra-warehouse-mohammadpur-delhi-warehouses-services-1o8sinohci-250.jpg;" alt="Myntra" style="height:300px; width:300px; margin-bottom:8px;">
-    <h2 style="color:#ff3f6c; margin-bottom:8px;">Verify Your Email</h2>
-    <p style="color:#333; font-size:16px; margin-bottom:24px;">
-      Thank you for signing up with Myntra!<br>
-      Please use the code below to verify your email address.
-    </p>
-    <div style="display:inline-block; background:#fff; color:#ff3f6c; font-size:28px; letter-spacing:6px; border:1px solid #ff3f6c; padding:12px 32px; border-radius:6px; font-weight:bold; margin-bottom:24px;">
-      ${verificationCode}
-    </div>
-    <p style="color:#555; font-size:14px; margin-top:24px;">
-      Didn’t request this? Please ignore this email.<br>
-      <b style="color:blue;">Need help?</b> Contact <a href="mailto:support@myntra.com" style="color:#ff3f6c; text-decoration:none;">support@myntra.com</a>
-    </p>
-  </div>
-</div>`
-  );
-
   //generating access and refresh token
   const { accessToken, refreshToken } = await generateTokens(user._id);
 
@@ -272,6 +270,28 @@ const register = asyncHandler(async (req, res) => {
     const verificationCode = Math.floor(
       100000 + Math.random() * 900000
     ).toString();
+    // Send verification email
+    await sendEmail(
+      email,
+      "Verify your email",
+      `<div style="max-width:400px; margin:0 auto;padding:28px 20px; background:#fff; border-radius:8px; box-shadow:0 2px 8px rgba(0,0,0,0.08); font-family:'Segoe UI',Arial,sans-serif;">
+  <div style="text-align:center;">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDbzpE2oQZB9Wmio6e_wnjbj1p4MNiTGCBLQ&s" alt="Myntra" style="height:300px; width:300px; margin-bottom:8px;">
+    <h2 style="color:#0b751bff; margin-bottom:8px;">Verify Your Email</h2>
+    <p style="color:#333; font-size:16px; margin-bottom:24px;">
+      Thank you for signing up with Myntra!<br>
+      Please use the code below to verify your email address.
+    </p>
+    <div style="display:inline-block; background:#fff; color:#0b751bff; font-size:28px; letter-spacing:6px; border:1px solid #0b751bff; padding:12px 32px; border-radius:6px; font-weight:bold; margin-bottom:24px;">
+      ${verificationCode}
+    </div>
+    <p style="color:#555; font-size:14px; margin-top:24px;">
+      Didn’t request this? Please ignore this email.<br>
+      <b style="color:blue;">Need help?</b> Contact <a href="mailto:support@college.com" style="color:#0b751bff; text-decoration:none;">support@myntra.com</a>
+    </p>
+  </div>
+</div>`
+    );
     //Create new user
 
     const user = await Registration.create({
@@ -344,32 +364,7 @@ const registerLogin = asyncHandler(async (req, res) => {
   if (!isPasswordValid) {
     throw new ApiError(401, "Invalid password or credentials");
   }
-  // Generate a random 6-digit verification code
-  const verificationCode = Math.floor(
-    100000 + Math.random() * 900000
-  ).toString();
-  // Send verification email
-  await sendEmail(
-    email,
-    "Verify your email",
-    `<div style="max-width:400px; margin:0 auto;padding:28px 20px; background:#fff; border-radius:8px; box-shadow:0 2px 8px rgba(0,0,0,0.08); font-family:'Segoe UI',Arial,sans-serif;">
-  <div style="text-align:center;">
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDbzpE2oQZB9Wmio6e_wnjbj1p4MNiTGCBLQ&s" alt="Myntra" style="height:300px; width:300px; margin-bottom:8px;">
-    <h2 style="color:#0b751bff; margin-bottom:8px;">Verify Your Email</h2>
-    <p style="color:#333; font-size:16px; margin-bottom:24px;">
-      Thank you for signing up with Myntra!<br>
-      Please use the code below to verify your email address.
-    </p>
-    <div style="display:inline-block; background:#fff; color:#0b751bff; font-size:28px; letter-spacing:6px; border:1px solid #0b751bff; padding:12px 32px; border-radius:6px; font-weight:bold; margin-bottom:24px;">
-      ${verificationCode}
-    </div>
-    <p style="color:#555; font-size:14px; margin-top:24px;">
-      Didn’t request this? Please ignore this email.<br>
-      <b style="color:blue;">Need help?</b> Contact <a href="mailto:support@college.com" style="color:#0b751bff; text-decoration:none;">support@myntra.com</a>
-    </p>
-  </div>
-</div>`
-  );
+
   //generating access and refresh token
   const { accessToken, refreshToken } = await generateRegisterTokens(user._id);
 
